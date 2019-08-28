bc8fadc639b7be6826eb976424ba31f922ad8cee
Guilherme H <guilherme.assis@luizalabs.com>
2019-08-28 18:19:11 -0300

    utils e enums e entidade Empresa

--- src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
+++ src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
@@ -1,14 +1,29 @@
 package br.com.unifacef.api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.com.unifacef.utils.SenhaUtils;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoSpringApplication.class, args);
-		System.out.println("Esta é uma alteração no projeto...");
+		System.out.println("Alteramos o arquivo application.properties ");
+		System.out.println("de modo que não precisemos configurar agora a fonte de dados do MySQL ");
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encosded: " + senhaEncoded);
+			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha encoded novamente:" + senhaEncoded);
+			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));
+		};
+	}
 }
