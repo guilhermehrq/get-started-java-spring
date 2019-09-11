f7d51c6be43d19bc1361b4d65920f365fd8154e8
Guilherme H <guilherme.assis@luizalabs.com>
2019-09-10 22:30:28 -0300

    conectando banco

--- src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
+++ src/main/java/br/com/unifacef/api/MeuPrimeiroProjetoSpringApplication.java
@@ -1,29 +1,57 @@
 package br.com.unifacef.api;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import br.com.unifacef.api.entities.Empresa;
 import br.com.unifacef.utils.SenhaUtils;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoSpringApplication {
-
+	@Autowired
+	private EmpresaRepository empresaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoSpringApplication.class, args);
-		System.out.println("Alteramos o arquivo application.properties ");
-		System.out.println("de modo que não precisemos configurar agora a fonte de dados do MySQL ");
+//		System.out.println("Alteramos o arquivo application.properties ");
+//		System.out.println("de modo que não precisemos configurar agora a fonte de dados do MySQL ");
 	}
 
 	@Bean
 	public CommandLineRunner commandLineRunner() {
 		return args -> {
-			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
-			System.out.println("Senha encosded: " + senhaEncoded);
-			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
-			System.out.println("Senha encoded novamente:" + senhaEncoded);
-			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));
+//			String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+//			System.out.println("Senha encosded: " + senhaEncoded);
+//			senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+//			System.out.println("Senha encoded novamente:" + senhaEncoded);
+//			System.out.println("Senha válida: " + SenhaUtils.senhaValida("123456", senhaEncoded));
+			
+			Empresa empresa = new Empresa();
+			empresa.setRazaoSocial("Kazale IT");
+			empresa.setCnpj("74645215000104");
+			this.empresaRepository.save(empresa);
+			
+			List<Empresa> empresas = empresaRepository.findAll();
+			empresas.forEach(System.out::println);
+			
+			Empresa empresaDb = empresaRepository.findById(1L).orElse(null);
+			System.out.println("Empresa por ID: " + empresaDb);
+			
+			empresaDb.setRazaoSocial("Kazale IT Web");
+			this.empresaRepository.save(empresaDb);
+			
+			Empresa empresaCnpj = empresaRepository.findByCnpj("74645215000104");
+			System.out.println("Empresa por CNPJ: " + empresaCnpj);
+			
+			this.empresaRepository.delete(empresaCnpj);
+			
+			empresas = empresaRepository.findAll();
+			System.out.println("Empresas: " + empresas.size());
 		};
 	}
 }
